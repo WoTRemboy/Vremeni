bab658075b0b79f00c9ba568a518d1629e022bb0
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-08-19 15:09:08 +0300

    Application module comments

--- Vremeni/Application/VremeniApp.swift
+++ Vremeni/Application/VremeniApp.swift
@@ -10,23 +10,35 @@ import SwiftData
 
 @main
 struct VremeniApp: App {
+    
+    // MARK: - Properties
+    
+    // Banner viewModel
     @StateObject private var bannerService = BannerViewModel()
+    // SwiftData container
     private let container: ModelContainer
     
+    // MARK: - Body view
     
     internal var body: some Scene {
         WindowGroup {
             ZStack {
+                // Main views
                 SplashScreenView(modelContext: container.mainContext)
                 
+                // Banner view
                 if let type = bannerService.bannerType {
                     BannerView(type: type)
                 }
             }
+            // Banner viewModel environment
             .environmentObject(bannerService)
         }
+        // SwiftData model container
         .modelContainer(container)
     }
+    
+    // MARK: - Initialization
     
     init() {
         do {
