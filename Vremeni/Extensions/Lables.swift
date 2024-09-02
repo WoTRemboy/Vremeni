2a5cd488f502da5ca8f04ca835d57fd834d15516
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-09-02 11:15:37 +0300

    machine page limit value localized

--- Vremeni/Extensions/Lables.swift
+++ Vremeni/Extensions/Lables.swift
@@ -111,6 +111,7 @@ final class Texts {
         
         static let upgrade = NSLocalizedString("MachinePageUpgrade", comment: "Upgrade")
         static let purchase = NSLocalizedString("MachinePagePurchase", comment: "Purchase")
+        static let limit = NSLocalizedString("MachinePageLimit", comment: "Limit has been reached")
         
         static let targetTime = NSLocalizedString("MachinePageTargetTime", comment: "Target time")
         static let potentialTime = NSLocalizedString("MachinePagePotentialTarget", comment: "Potential target")
